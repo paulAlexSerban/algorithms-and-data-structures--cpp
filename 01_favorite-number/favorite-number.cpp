800813cc6b2b10c5973d794b30d18eb202407bf8
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-25 15:54:46 +0300

    first user input

---
+++ 01_favorite-number/favorite-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int favorite_number = 0;
+
+  std::cout << "Enter your favorite number between 1 an 100: ";
+  std::cin >> favorite_number;
+  std::cout << "Amazing! That is my favorite number too!" << std::endl;
+  return 0; 
+}
